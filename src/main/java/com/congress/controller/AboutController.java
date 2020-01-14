22f5f39ee411852821eeaa9511d016525aaf7f38
mixheaven <mixheaven@hotmail.com>
2020-01-14 09:19:03 +0100

    debut de la table About

--- src/main/java/com/congress/controller/AboutController.java
+++ src/main/java/com/congress/controller/AboutController.java
@@ -1,11 +1,15 @@
 package com.congress.controller;
 
+import com.congress.entity.About;
 import com.congress.repository.AboutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 
 @Controller
@@ -23,9 +27,42 @@ public class AboutController {
      * @return Template of about view list
      */
     @GetMapping
-    public String getAbout(Model model){
+    public String getAbout(Model model) {
         model.addAttribute("aboutList", aboutRepository.findAll());
         model.addAttribute("pageTitle", "About");
         return "/pages/about/aboutListView";
+    }
+
+    /**
+     * this controller display a specific description in about
+     */
+    @GetMapping("/{id}")
+    public String getAbout(@PathVariable long id, Model model) throws Exception {
+        Optional<About> finded = aboutRepository.findById(id);
+        About titleAbout = finded.get();
+        if (finded.isPresent()) {
+            titleAbout = finded.get();
+        } else {
+            throw new Exception("Can't find description, please write description ");
+            model.addAttribute("titleAbout", titleAbout);
+            model.addAttribute("pageTitle", "About" + titleAbout.getTitle());
+            return "pages/about/aboutOneDescription";
+        }
+
+        /**
+         * this controller is used to create description in About
+         *
+         */
+        @PostMapping
+        public String createAbout (@Valid @ModelAttribute About titleAbout, BindingResult bindingAbout, Model model){
+            if (bindingAbout.hasErrors()) {
+                model.addAttribute("httpMethod", "POST");
+                model.addAttribute("pathMethod", "/about");
+                model.addAttribute("newAbout", titleAbout);
+                return "pages/about/aboutFormView";
+            }
+            titleAbout = aboutRepository.save(titleAbout);
+            return "redirect:/about/" + titleAbout.getId();
+        }
     }
 }
