5209cee56ae4bc19ea845e22db1b90510298c528
mixheaven <mixheaven@hotmail.com>
2020-01-19 18:30:39 +0100

    controller sponsors

---
+++ src/main/java/com/congress/entity/Sponsors.java
@@ -0,0 +1,112 @@
+package com.congress.entity;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.*;
+
+@Entity
+public class Sponsors {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String logo_url;
+    @Transient
+    private MultipartFile logo;
+    private String location;
+    private String city;
+    private String contact;
+    private String adress;
+    private int phone;
+    private String mail;
+    private String website_url;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogo_url() {
+        return logo_url;
+    }
+
+    public void setLogo_url(String logo_url) {
+        this.logo_url = logo_url;
+    }
+
+    public MultipartFile getLogo() {
+        return logo;
+    }
+
+    public void setLogo(MultipartFile logo) {
+        this.logo = logo;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getWebsite_url() {
+        return website_url;
+    }
+
+    public void setWebsite_url(String website_url) {
+        this.website_url = website_url;
+    }
+}
---
+++ src/main/java/com/congress/repository/SponsorsRepository.java
@@ -0,0 +1,7 @@
+package com.congress.repository;
+
+import com.congress.entity.Sponsors;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SponsorsRepository extends JpaRepository<Sponsors, Long> {
+}
