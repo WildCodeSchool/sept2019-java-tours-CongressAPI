9981821dda0e458581e16e1f65a0f7a77d3405ee
mixheaven <mixheaven@hotmail.com>
2020-01-21 15:51:02 +0100

    fichier Sponsorts controller

--- src/main/java/com/congress/controller/SponsortsController.java
+++ src/main/java/com/congress/controller/SponsortsController.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import javax.validation.Valid;
 import java.util.Optional;
