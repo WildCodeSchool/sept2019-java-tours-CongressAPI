14fbb8693fdfdd094fe002b4f54f73ee352e8c6c
mixheaven <mixheaven@hotmail.com>
2020-01-31 09:57:08 +0100

    fin css

--- src/main/java/com/congress/controller/AppController.java
+++ src/main/java/com/congress/controller/AppController.java
@@ -4,6 +4,7 @@ import com.congress.storage.StorageFileNotFoundException;
 import com.congress.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -42,6 +43,12 @@ public class AppController {
     public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
         return ResponseEntity.notFound().build();
     }
-
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(  HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
 
 }
